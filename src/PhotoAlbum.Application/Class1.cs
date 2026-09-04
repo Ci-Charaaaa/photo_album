4e10488dab82267ead3a1c39a5b06496f1d445e0
吴丕博 <13942694716@163.com>
2026-09-04 21:14:23 +0800

    创建Application的模板，完善Domain层，修改REAMME

--- src/PhotoAlbum.Application/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace PhotoAlbum.Application;
-
-public class Class1
-{
-
-}
--- src/PhotoAlbum.Domain/Interfaces/IStorageBackend.cs
+++ src/PhotoAlbum.Domain/Interfaces/IStorageBackend.cs
@@ -4,13 +4,13 @@
 public interface IStorageBackend
 {
     // 基础方法，目录创建，目录删除，
-    // 文件删除，文件移动，读取文件流（用于照片读），
-    // 文件是否存在，写入字节流，读取字节流（两个字节流用于缩略图读写）
     public Task CreateDirectoryAsync(string path);
     public Task DeleteDirectoryAsync(string path);
+    // 文件删除，文件移动，读取文件流（用于照片读）
     public Task DeleteFileAsync(string path);
     public Task MoveFileAsync(string src,string dst);
     public Task<bool> ExistsAsync(string path);
+    // 文件是否存在，写入字节流，读取字节流（两个字节流用于缩略图读写）
     public Task<Stream> ReadFileAsync(string path);
     public Task WriteBytesAsync(string path, byte[] bytes);
     public Task<byte[]> ReadBytesAsync(string path);
